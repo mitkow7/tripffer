8588aaf3d8000082ae661d96bca8390516a696b4
mitkow7 <kingdogbg2@gmail.com>
2025-08-04 17:40:32 +0300

    fix: fixing AWS servers

--- backend/backend/storage.py
+++ backend/backend/storage.py
@@ -42,7 +42,7 @@ class MediaStorage(S3Boto3Storage):
         return None
 
 # Media files configuration
-if all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_CUSTOM_DOMAIN]):
+if all([AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME]):
     # Use S3 for media storage
     DEFAULT_FILE_STORAGE = 'backend.storage.MediaStorage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
